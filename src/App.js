4ecee0c0203852d234d4e8d0a7c43366a8f2ddf7
stargood111 <stargood37@naver.com>
2024-07-03 00:17:00 +0900

    Routing, navigate, nested, routes, outlet

--- src/App.js
+++ src/App.js
@@ -6,21 +6,23 @@ import Container from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import {useState} from "react";
 import data from './data.js';
-import {Routes, Route, Link} from 'react-router-dom'
-import Detail from './Detail.js'
+import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
+// useNavigate = Link 와 같으나 a태그를 없애주고 -1,-2 +1 같은 기능이 있음
+// Outlet 원하는 404페이지 같은 경로 설정 가능
+import Detail from './pages/Detail.js'
 function App(){
 
   let [shoes] = useState(data);
-
+  let navigate = useNavigate();
   return (
       <div>
 
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="grey" variant="grey">
           <Container>
             <Navbar.Brand href="#home">Navbar</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link onClick={()=> {navigate(-1)}}>Home</Nav.Link>
+              <Nav.Link onClick={()=>{navigate('/detail')}}>Detail</Nav.Link>
               <Nav.Link href="#pricing">Pricing</Nav.Link>
             </Nav>
           </Container>
@@ -41,12 +43,14 @@ function App(){
                  </>
               }>
               </Route>
-              <Route path="/detail" element={<Detail/>}>
+              <Route path="/detail/:id" element={<Detail shoes ={shoes} />}></Route>
+              {/*<Route path="/*" element={<div>잘못된 경로입니다.</div>}></Route>*/}
+              {/*<Route path="about" element={<div>어바웃페이지</div>}></Route> //Outlet Hook*/}
+              <Route path="/about" element={<About/>}>
+                  <Route path="member" element={<About/>}></Route> ///about/member
+                  <Route path="location" element={<About/>}></Route> ///about/location
               </Route>
-              <Route path="about" element={<div>어바웃페이지</div>}></Route>
           </Routes>
-
-
       </div>
   )
 }
@@ -64,5 +68,12 @@ const Modal = (props) => {
         </>
     );
 }
+const About = () => {
+                return(
+                    <div>
+                        <h4>회사정보</h4>
+                    </div>
+                )
+            }
 
 export default App;
---
+++ src/pages/Detail.js
@@ -0,0 +1,27 @@
+import {Route, useParams} from "react-router-dom";
+import {useState} from "react";
+
+
+const Detail = (props) => {
+
+    let {id} = useParams(); // url Parmeter 정보   <Route path="/detail/:id" element={<Detail shoes ={shoes} />}></Route>
+    const filterNum = /^\d+$/.test(id);     // id가 숫자로만 구성된 문자열인지를 체크하는 정규 표현식
+
+    let[shoes] = useState();
+    return(
+        filterNum ?
+            <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                    <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%"/>
+                </div>
+                <div className="col-md-6">
+                    <h4 className="pt-5">{props.shoes[0].title}</h4>
+                    <p>상품설명</p>
+                    <p>120000원</p>
+                    <button className="btn btn-danger">주문하기</button>
+                </div>
+            </div>
+        </div> : <div>잘못되었슴</div>
+            )}
+export default Detail;
