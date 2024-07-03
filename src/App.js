94dd00ef249e7481de83a8a0dd7c886bb4ecf8b3
stargood111 <stargood37@naver.com>
2024-07-04 00:59:47 +0900

    useEffect, axios

--- src/App.js
+++ src/App.js
@@ -10,40 +10,50 @@ import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
 // useNavigate = Link 와 같으나 a태그를 없애주고 -1,-2 +1 같은 기능이 있음
 // Outlet 원하는 404페이지 같은 경로 설정 가능
 import Detail from './pages/Detail.js'
+import styled from 'styled-components'
+import axios from "axios";
+
 function App(){
 
-  let [shoes] = useState(data);
+  let [shoes, setShoes] = useState(data);
+  let [shoes2, setShoes2] = useState()
   let navigate = useNavigate();
+  let [click, setClick] = useState(0);
+  let [click2 ,setClick2] =useState(true);
   return (
       <div>
 
-        <Navbar bg="grey" variant="grey">
-          <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link onClick={()=> {navigate(-1)}}>Home</Nav.Link>
-              <Nav.Link onClick={()=>{navigate('/detail')}}>Detail</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
+          <Navbar bg="grey" variant="grey">
+              <Container>
+                  <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                  <Nav className="me-auto">
+                      <Nav.Link onClick={() => {
+                          navigate(-1)
+                      }}>Home</Nav.Link>
+                      <Nav.Link onClick={() => {
+                          navigate('/detail')
+                      }}>Detail</Nav.Link>
+                      <Nav.Link href="#pricing">Pricing</Nav.Link>
+                  </Nav>
+              </Container>
+          </Navbar>
           <Link to="/">Home</Link>&nbsp;&nbsp;&nbsp;
           <Link to="/detail">Detail</Link>&nbsp;&nbsp;&nbsp;
-          <Link to ="/about">About</Link>&nbsp;&nbsp;&nbsp;
+          <Link to="/about">About</Link>&nbsp;&nbsp;&nbsp;
           <Routes>
               <Route path="/" element={
                   <>
-                  <div className="main-bg">
-                  </div>
-                  <div className="container">
-                    <div className="row">
-                      <Modal shoes = {shoes}></Modal>
-                    </div>
-                  </div>
-                 </>
+                      <div className="main-bg">
+                      </div>
+                      <div className="container">
+                          <div className="row">
+                              <Modal shoes={shoes}></Modal>
+                          </div>
+                      </div>
+                  </>
               }>
               </Route>
-              <Route path="/detail/:id" element={<Detail shoes ={shoes} />}></Route>
+              <Route path="/detail/:id" element={<Detail shoes={shoes}/>}></Route>
               {/*<Route path="/*" element={<div>잘못된 경로입니다.</div>}></Route>*/}
               {/*<Route path="about" element={<div>어바웃페이지</div>}></Route> //Outlet Hook*/}
               <Route path="/about" element={<About/>}>
@@ -51,6 +61,22 @@ function App(){
                   <Route path="location" element={<About/>}></Route> ///about/location
               </Route>
           </Routes>
+          { click2 == true ?
+          <button onClick={() => {
+              if(click < 2) {
+              axios.get('https://codingapple1.github.io/shop/data2.json').then((response) => {
+                  let resultResponse = [...shoes, ...response.data]
+                  setShoes(resultResponse)
+                  setClick(click + 1)
+              })
+                  .catch(() => {
+                      console.log('실패함')
+                  })}
+              else{
+                  setClick2(false)
+                  alert('더는 없습니다')
+          }}}>더보기
+          </button> : null }
       </div>
   )
 }
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,27 +1,40 @@
 import {Route, useParams} from "react-router-dom";
-import {useState} from "react";
-
+import {useEffect, useState} from "react";
 
 const Detail = (props) => {
-
+    let [test, setTest] = useState(1);
+    let [test2,  setTest2] = useState(1);
     let {id} = useParams(); // url Parmeter 정보   <Route path="/detail/:id" element={<Detail shoes ={shoes} />}></Route>
-    const filterNum = /^\d+$/.test(id);     // id가 숫자로만 구성된 문자열인지를 체크하는 정규 표현식
-
     let[shoes] = useState();
+    let [warning ,setWarning] = useState('');
+    let [box, setBox] = useState(false);
+
+    useEffect(() => {
+        if(isNaN(warning) == true){
+            alert('그러지마세요')
+        }
+    }, [warning]);
+
     return(
-        filterNum ?
-            <div className="container">
-            <div className="row">
+        <div className="container">
+            {props.shoes[id] != null ?
+                <div className="row">
+                <input type="text" placeholder="수량입력" onChange={(e)=>{
+                    setWarning(e.target.value)
+                }}/>
                 <div className="col-md-6">
-                    <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%"/>
+                    <img src={`https://codingapple1.github.io/shop/shoes${parseInt(id) + 1}.jpg`} width="100%"/>
                 </div>
                 <div className="col-md-6">
-                    <h4 className="pt-5">{props.shoes[0].title}</h4>
-                    <p>상품설명</p>
-                    <p>120000원</p>
-                    <button className="btn btn-danger">주문하기</button>
-                </div>
-            </div>
-        </div> : <div>잘못되었슴</div>
-            )}
+                <h4 className="pt-5">{props.shoes[id].title}</h4>
+    <p>상품설명</p>
+    <p>120000원</p>
+    <button className="btn btn-danger">주문하기</button>
+</div>
+</div> :
+    <div>없는 데이터 입니다</div>}
+        </div>
+    )
+}
+
 export default Detail;
